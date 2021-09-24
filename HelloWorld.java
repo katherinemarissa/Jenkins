8635d3f43c50affb8b7254a7459697d0e912eead
katherinemarissa <81766361+katherinemarissa@users.noreply.github.com>
2021-09-24 16:42:24 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World. This is an edit 123.");
+		System.out.println("Hello World. My edit.");
 	}
 	
 }
